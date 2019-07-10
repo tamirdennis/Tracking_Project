6739a00dc25242332afe020bd52733a20c1485d9
tamir <tamir.d@futurelearning.ai>
2019-07-10 12:50:48 +0300

    init repo

---
+++ track.py
@@ -0,0 +1,146 @@
+from enum import Enum
+import numpy as np
+from filterpy.kalman import KalmanFilter
+
+
+class TrackStatus(Enum):
+    Confirmed = 2
+    Deleted = 3
+    Tentative = 1
+
+
+class Track(object):
+
+    count = 0
+
+    def __init__(self, init_state, n_init, max_age, feature=None):
+        self.curr_state = init_state
+        self.id = Track.count
+        Track.count += 1
+        self.hits = 0
+
+        self._max_age = max_age
+        self._n_init = n_init
+
+        self.features = []
+        if feature is not None:
+            self.features.append(feature)
+        self.age = 0
+        self.time_since_update = 0
+        self.status = TrackStatus.Tentative
+        self.hit_streak = 0
+
+    def __del__(self):
+        Track.count -= 1
+
+
+    def is_confirmed(self):
+        return self.status == TrackStatus.Confirmed
+
+    def is_deleted(self):
+        return self.status == TrackStatus.Deleted
+
+    def is_tentative(self):
+        return self.status == TrackStatus.Tentative
+
+    def mark_missed(self):
+        self.time_since_update += 1
+        self.age += 1
+
+    def predict(self):
+        if (self.time_since_update > 0):
+            self.hit_streak = 0
+        self.time_since_update += 1
+        self.age += 1
+        return self.get_state()
+
+    def update(self, detection):
+        self.time_since_update = 0
+        self.hits += 1
+        self.hit_streak += 1
+        self.curr_state = detection
+
+    def get_state(self):
+        return self.curr_state
+
+def convert_bbox_to_z(bbox):
+    """
+    Takes a bounding box in the form [x1,y1,x2,y2] and returns z in the form
+      [x,y,s,r] where x,y is the centre of the box and s is the scale/area and r is
+      the aspect ratio
+    """
+    w = bbox[2] - bbox[0]
+    h = bbox[3] - bbox[1]
+    x = bbox[0] + w / 2.
+    y = bbox[1] + h / 2.
+    s = w * h  # scale is just area
+    r = w / float(h)
+    return np.array([x, y, s, r]).reshape((4, 1))
+
+
+def convert_x_to_bbox(x, score=None):
+    """
+    Takes a bounding box in the centre form [x,y,s,r] and returns it in the form
+      [x1,y1,x2,y2] where x1,y1 is the top left and x2,y2 is the bottom right
+    """
+    w = np.sqrt(x[2] * x[3])
+    h = x[2] / w
+    if (score == None):
+        return np.array([x[0] - w / 2., x[1] - h / 2., x[0] + w / 2., x[1] + h / 2.]).reshape((1, 4))
+    else:
+        return np.array([x[0] - w / 2., x[1] - h / 2., x[0] + w / 2., x[1] + h / 2., score]).reshape((1, 5))
+
+
+class KalmanTrack(Track):
+    """
+    This class represents the internel state of individual tracked objects observed as bbox.
+    """
+
+    def __init__(self, init_state, n_init, max_age):
+        """
+        Initialises a tracker using initial bounding box.
+        """
+        # define constant velocity model
+
+        super(KalmanTrack, self).__init__(init_state, n_init, max_age)
+        self.kf = KalmanFilter(dim_x=7, dim_z=4)
+        self.init_kalman_filter(init_state)
+
+    def init_kalman_filter(self, init_state):
+        self.kf.F = np.array(
+            [[1, 0, 0, 0, 1, 0, 0], [0, 1, 0, 0, 0, 1, 0], [0, 0, 1, 0, 0, 0, 1], [0, 0, 0, 1, 0, 0, 0],
+             [0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 1]])
+        self.kf.H = np.array(
+            [[1, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0]])
+
+        self.kf.R[2:, 2:] *= 10.
+        self.kf.P[4:, 4:] *= 1000.  # give high uncertainty to the unobservable initial velocities
+        self.kf.P *= 10.
+        self.kf.Q[-1, -1] *= 0.01
+        self.kf.Q[4:, 4:] *= 0.01
+
+        self.kf.x[:4] = convert_bbox_to_z(init_state)
+
+    def update(self, bbox):
+        """
+        Updates the state vector with observed bbox.
+        """
+        super(KalmanTrack, self).update(bbox)
+        self.kf.update(convert_bbox_to_z(bbox))
+
+    def predict(self):
+        """
+        Advances the state vector and returns the predicted bounding box estimate.
+        """
+        super(KalmanTrack, self).predict()
+        if ((self.kf.x[6] + self.kf.x[2]) <= 0):
+            self.kf.x[6] *= 0.0
+        self.kf.predict()
+
+        return convert_x_to_bbox(self.kf.x)
+
+    def get_state(self):
+        """
+        Returns the current bounding box estimate.
+        """
+        return convert_x_to_bbox(self.kf.x)
---
+++ tracker.py
@@ -0,0 +1,113 @@
+import numpy as np
+from filterpy.kalman import KalmanFilter
+from metric import Metric
+from sklearn.utils.linear_assignment_ import linear_assignment
+from track import Track, KalmanTrack
+
+class Tracker(object):
+
+    def __init__(self, metric_str, metric_threshold=None, max_age=30, n_init=3):
+        
+        self.frame_count = 0
+        self.tracks = []
+        self.max_age = max_age
+        self.n_init = n_init
+        self.metric = Metric(metric_str)
+        if metric_threshold is None:
+            self.metric_threshold = self.metric.threshold
+        else:
+            self.metric_threshold = metric_threshold
+
+    def associate_detections_to_trackers(self, detections, trackers):
+        """
+        Assigns detections to tracked object (both represented as bounding boxes)
+        
+        Returns 3 lists of matches, unmatched_detections and unmatched_trackers
+        """
+        if len(trackers) == 0:
+            return np.empty((0, 2), dtype=int), np.arange(len(detections)), np.empty((0, 5), dtype=int)
+
+        dist_matrix = self.metric.distance(detections, trackers)
+
+        if self.metric_threshold < 0:
+            dist_matrix = -dist_matrix
+        matched_indices = linear_assignment(dist_matrix)
+
+        unmatched_trackers = []
+        unmatched_detections = []
+        if len(detections) > len(trackers):
+            unmatched_detections = {i for i in xrange(len(detections))}
+            matched_det_set = set(matched_indices[:, 0])
+            unmatched_detections.difference_update(matched_det_set)
+            unmatched_detections = list(unmatched_detections)
+
+        else:
+            unmatched_trackers = {i for i in xrange(len(trackers))}
+            matched_trk_set = set(matched_indices[:, 1])
+            unmatched_trackers.difference_update(matched_trk_set)
+            unmatched_trackers = list(unmatched_trackers)
+
+        matches = []
+        for m in matched_indices:
+            if dist_matrix[m[0], m[1]] > self.metric_threshold:
+                unmatched_detections.append(m[0])
+                unmatched_trackers.append(m[1])
+            else:
+                matches.append(m.reshape(1, 2))
+        if len(matches) == 0:
+            matches = np.empty((0, 2), dtype=int)
+        else:
+            matches = np.concatenate(matches, axis=0)
+        
+        return matches, np.array(unmatched_detections), np.array(unmatched_trackers)
+
+    def predict(self):
+        self.frame_count += 1
+        # get predicted locations from existing trackers.
+        trks = np.zeros((len(self.tracks), 5))
+        to_del = []
+        for t, trk in enumerate(trks):
+            pos = self.tracks[t].predict()[0]
+            trk[:] = [pos[0], pos[1], pos[2], pos[3], 0]
+            if (np.any(np.isnan(pos))):
+                to_del.append(t)
+        trks = np.ma.compress_rows(np.ma.masked_invalid(trks))
+        for t in reversed(to_del):
+            self.tracks.pop(t)
+        return trks
+
+    def update(self, dets):
+        """
+        Params:
+          dets - a numpy array of detections in the format [[x1,y1,x2,y2,score],[x1,y1,x2,y2,score],...]
+        Requires: this method must be called once for each frame even with empty detections.
+        Returns the a similar array, where the last column is the object ID.
+
+        NOTE: The number of objects returned may differ from the number of detections provided.
+        """
+        trks = self.predict()
+        ret = []
+        matched, unmatched_dets, unmatched_trks = self.associate_detections_to_trackers(dets, trks)
+
+        # update matched trackers with assigned detections
+        for t, trk in enumerate(self.tracks):
+            if (t not in unmatched_trks):
+                d = matched[np.where(matched[:, 1] == t)[0], 0]
+                trk.update(dets[d, :][0])
+
+        # create and initialise new trackers for unmatched detections
+        for i in unmatched_dets:
+            trk = KalmanTrack(dets[i, :], self.n_init, self.max_age)
+            self.tracks.append(trk)
+        i = len(self.tracks)
+        for trk in reversed(self.tracks):
+            d = trk.get_state()[0]
+            if ((trk.time_since_update < 1) and (trk.hit_streak >= self.n_init or self.frame_count <= self.n_init)):
+                ret.append(np.concatenate((d, [trk.id + 1])).reshape(1, -1))  # +1 as MOT benchmark requires positive
+            i -= 1
+            # remove dead tracklet
+            if (trk.time_since_update > self.max_age):
+                self.tracks.pop(i)
+        if (len(ret) > 0):
+            return np.concatenate(ret)
+        return np.empty((0, 5))
